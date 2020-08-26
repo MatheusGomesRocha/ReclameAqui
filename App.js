ba25787dff237427d3934ef329237be41a9418dc
Matheus <matheusgomes192@hotmail.com>
2020-08-26 17:45:53 -0300

    criando LoginScreen e finalizando seu front end

--- App.js
+++ App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'react-native';
 import AppTab from './src/navigators/AppTab';
+import L from './src/screens/LoginScreen';
 
 export default () => {
   return(
-      <NavigationContainer>
-        <AppTab />
-        <StatusBar backgroundColor="#5E239D" barStyle="light-content"/>
-      </NavigationContainer>
+      <L />
   );
 }
