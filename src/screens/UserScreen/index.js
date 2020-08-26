d6651747c510aa998e3582db41e4da8a025db6ff
Matheus <matheusgomes192@hotmail.com>
2020-08-26 17:25:39 -0300

    finalizando front end da UserScreen

--- src/screens/UserScreen/index.js
+++ src/screens/UserScreen/index.js
@@ -1,16 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import {
     Container,
+    Scroll,
+    UserView,
+    UserEmail,
+    DefaultView,
+    ItemBtn,
+    DefaultText
 
-    Texto,
-    Input,
+    
 } from './style';
 
 export default () => {
+    const [user, setUser] = useState(false);            // Temporário, trocar para CurrentUser do Firebase
+
+    
     return(
         <Container>
-            <Texto> Olá mundo user </Texto>
+            <UserView>
+                <Icon name="user-circle" color="#fff" size={70} />
+                <UserEmail> matheusgomes192 </UserEmail>
+            </UserView>
+
+            <DefaultView>
+                <ItemBtn>
+                    <>
+                        <DefaultText> Faça Login </DefaultText> 
+                        <Icon name="angle-right" size={30} color="#fff" />
+                    </>
+                </ItemBtn>
+                {user?
+                    <>
+                        <ItemBtn>
+                            <>
+                                <DefaultText> Meu Perfil </DefaultText> 
+                                <Icon name="angle-right" size={30} color="#fff" />
+                            </>
+                        </ItemBtn>
+                        <ItemBtn>
+                            <>
+                                <DefaultText> Histórico </DefaultText> 
+                                <Icon name="angle-right" size={30} color="#fff" />
+                            </>
+                        </ItemBtn>
+                        <ItemBtn>
+                            <>
+                                <DefaultText> Sair </DefaultText> 
+                                <Icon name="angle-right" size={30} color="#fff" />
+                            </>
+                        </ItemBtn>
+                    </>
+                : null}
+
+            </DefaultView>
+
         </Container>
     );
 }
--- src/screens/UserScreen/style.js
+++ src/screens/UserScreen/style.js
@@ -4,12 +4,36 @@ export const Container = styled.SafeAreaView`
     flex: 1;
     background-color: #5E239D;
     align-items: center;
-    justify-content: center;
 `;
+export const Scroll = styled.ScrollView``;
 
-export const Texto = styled.Text`
+export const UserView = styled.View`
+    height: 250px;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    borderBottomWidth: 1px;
+    borderBottomColor: #aaa;
+`;
+export const UserEmail = styled.Text`
     color: #fff;
     font-size: 24px;
+    margin-top: 15px;
 `;
 
-export const Input = styled.TextInput``;
+export const DefaultView = styled.View`
+    width: 100%;
+`;
+export const ItemBtn = styled.TouchableHighlight`
+    borderBottomWidth: 1px;
+    borderBottomColor: #aaa;
+    height: 80px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 10px 0 10px;
+`;
+export const DefaultText = styled.Text`
+    font-size: 18px;
+    color: #fff;
+`;
