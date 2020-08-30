0ed1578631b157bbb632bc7c22a391bd03c1fd41
Matheus <matheusgomes192@hotmail.com>
2020-08-30 17:12:35 -0300

    criando função de adicionar comentário para o firebase

--- src/screens/CommentsScreen/index.js
+++ src/screens/CommentsScreen/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 import {
     Container,
@@ -11,22 +13,44 @@ import {
 } from './style';
 
 export default () => {
+    const [placeName, setPlaceName] = useState();
+    const [userName, setUserName] = useState();
+    const [comment, setComment] = useState();
+    
+    const user = auth().currentUser;    // Pega o usuário logado (que acabou de logar junto com o cadastro)
+
+    function DoComment() {
+        if(placeName && comment) {
+            setUserName('0')
+            firestore()
+            .collection('comments')
+            .add({
+                userId: user.uid,
+                placeName: placeName,
+                userName: userName,
+                comment: comment,
+            })
+            .then(() => {
+                alert('Comentário feito com sucesso');
+            })
+    }
+
     return(
         <Container>
             <Scroll>
                 <InputView>
                     <Label> Nome do local </Label>
-                    <Input placeholder="Empresa, Lugar, Restaurantes..." placeholderTextColor="#bbb"/>
+                    <Input placeholder="Empresa, Lugar, Restaurantes..." placeholderTextColor="#bbb" onChangeText={pn=>setPlaceName(pn)} />
                 </InputView>
                 <InputView>
                     <Label> Seu nome </Label>
-                    <Input placeholder="Deixe vazio caso prefira anônimato" placeholderTextColor="#bbb"/>
+                    <Input placeholder="Deixe vazio caso prefira anônimato" placeholderTextColor="#bbb" onChangeText={un=>setUserName(un)}/>
                 </InputView>
                 <InputView>
                     <Label> Comentário </Label>
-                    <Input multiline style={{height: 250, textAlign: 'center'}} placeholder="Escreva aqui seu comentário" placeholderTextColor="#bbb"/>
+                    <Input onChangeText={c=>setComment(c)} multiline style={{height: 250, textAlign: 'center'}} placeholder="Escreva aqui seu comentário" placeholderTextColor="#bbb"/>
                 </InputView>
-                <Btn underlayColor="#C50750" onPress={() => alert('olá mundo')}>
+                <Btn underlayColor="#C50750" onPress={() => DoComment()}>
                     <BtnText> Finalizar </BtnText>
                 </Btn>
             </Scroll>
